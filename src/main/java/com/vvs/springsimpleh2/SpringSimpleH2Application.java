5baf402c144873031aae93cca532e652d5e36501
Victor <victorsmirnov@me.com>
2022-08-29 12:30:20 +0300

    add initial records in database

--- src/main/java/com/vvs/springsimpleh2/SpringSimpleH2Application.java
+++ src/main/java/com/vvs/springsimpleh2/SpringSimpleH2Application.java
@@ -1,7 +1,15 @@
 package com.vvs.springsimpleh2;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.vvs.springsimpleh2.model.User;
+import com.vvs.springsimpleh2.repository.UserRepository;
 
 @SpringBootApplication
 public class SpringSimpleH2Application {
@@ -10,4 +18,28 @@ public class SpringSimpleH2Application {
 		SpringApplication.run(SpringSimpleH2Application.class, args);
 	}
 
+	// Create init users in database
+	@Bean
+	ApplicationRunner init(UserRepository userRepository) {
+		return args -> {
+
+			List<User> users = new ArrayList<>();
+
+			User user01 = User.builder()
+				.username("user")
+				.password("password")
+				.email("user@mail.me")
+			.build();
+			User user02 = User.builder()
+				.username("victor")
+				.password("victor")
+				.email("victor@mail.me")
+			.build();
+
+			users.add(user01);
+			users.add(user02);
+			
+			userRepository.saveAll(users);
+		};
+	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
-CREATE TABLE IF NOT EXISTS `users` (id INT, username VARCHAR(250), password VARCHAR(250), email VARCHAR(250));
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY, 
+  username VARCHAR(250), 
+  password VARCHAR(250), 
+  email VARCHAR(250)
+);
